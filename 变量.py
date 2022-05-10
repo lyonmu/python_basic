b9353d90405d76c08fa03f242f7c8836fdbda11e
uqingu <uqingu@foxmail.com>
2022-05-10 21:48:28 +0800

    变量

---
+++ 变量.py
@@ -0,0 +1,26 @@
+# 单行注释
+
+# 双引号和单引号的工作机制一样
+def test():
+    # int类型可以表示无穷大的数字
+    variable1 = 123
+    variable2 = 11111111111111111111111111111111111111111111111111
+    variable3 = 123.23
+    variable4 = 'string'
+    # type 函数可以获取变量的类型
+    print(type(variable1))
+    print(type(variable2))
+    print(type(variable3))
+    print(type(variable4))
+
+
+if __name__ == '__main__':
+    test()
+
+'''
+多行注释
+多行注释
+多行注释
+多行注释
+多行注释
+'''
